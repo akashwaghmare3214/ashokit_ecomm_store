16052f8d4bbb00236e254d16ca95698a13b9a6c9
akashwaghmare3214 <akashwaghmare2499@gmail.com>
2025-11-16 18:13:42 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a704cc94d0d7649da8a5484c373c0106-283151249.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a00ce264bf181452d9c0e12f2bce3e18-1711830524.eu-west-1.elb.amazonaws.com',
 };
